a4008e4aad0eb49b529831eeaefed49d5d2d1229
ardalan.shahandeh <ardalan.shahandeh@gmail.com>
2023-10-06 01:26:28 +0330

    style: Crating Name Card

--- app.vue
+++ app.vue
@@ -130,13 +130,15 @@ const selectedNames = ref<string[]>([]);
       <button class="primary" @click="computSelectedNames">Find Name's</button>
     </div>
 
-    <button
-      class="result"
-      v-for="selectedName in selectedNames"
-      :key="selectedName"
-    >
-      {{ selectedName }}
-    </button>
+    <div class="card-container">
+      <div v-for="name in selectedNames" :key="name" class="card">
+        <h4>
+          {{ name }}
+        </h4>
+
+        <p>x</p>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -202,14 +204,26 @@ const selectedNames = ref<string[]>([]);
   margin-top: 1rem;
   cursor: pointer;
 }
-.result {
+.card-container {
+  display: flex;
+  margin-top: 3rem;
+  flex-wrap: wrap;
+}
+.card {
   background-color: rgb(27, 60, 138);
+  width: 28%;
   color: white;
-  border-radius: 2rem;
-  border: none;
-  padding: 1rem;
-  font-size: 0.75rem;
-  margin: 2rem 0.5rem;
+  border-radius: 1rem;
+  padding: 0.1rem;
+  margin-right: 0.5rem;
+  margin-bottom: 1rem;
+  position: relative;
+}
+.card p {
+  position: absolute;
+  top: -29%;
+  left: 92.5%;
   cursor: pointer;
+  color: rgba(255, 255, 255, 0.178);
 }
 </style>
