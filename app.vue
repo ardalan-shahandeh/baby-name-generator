91e188850b2ae94d5dc5895739e693972c2bba8d
ardalan.shahandeh <ardalan.shahandeh@gmail.com>
2023-10-06 14:46:43 +0330

    feat:Create Component And Clean Code

--- app.vue
+++ app.vue
@@ -13,18 +13,6 @@ const options = reactive<OptionsState>({
   popularity: Popularity.TRENDY,
 });
 
-function setLength(value: string) {
-  options.length = value;
-}
-
-function setGender(value: string) {
-  options.gender = value;
-}
-
-function setPopularity(value: string) {
-  options.popularity = value;
-}
-
 function computSelectedNames() {
   const filterNames = names
     .filter((name) => name.gender === options.gender)
@@ -38,6 +26,24 @@ function computSelectedNames() {
 }
 
 const selectedNames = ref<string[]>([]);
+
+const optionArray = [
+  {
+    title: "1)Choose a gender",
+    category: "gender",
+    buttons: [Gender.GIRL, Gender.UNISEX, Gender.BOY],
+  },
+  {
+    title: "2) Choos the name's popularity",
+    category: "popularity",
+    buttons: [Popularity.TRENDY, Popularity.UNIQUE],
+  },
+  {
+    title: "3) Choos name length",
+    category: "length",
+    buttons: [Length.SHORT, Length.ALL, Length.LONG],
+  },
+];
 </script>
 
 <template>
@@ -45,87 +51,12 @@ const selectedNames = ref<string[]>([]);
     <h1>Baby Name Generator</h1>
     <p>Choose your options and click the "Find Name " buttom below</p>
     <div class="options-container">
-      <div class="option-container">
-        <h4>1) Choose a gender</h4>
-
-        <div class="option-buttons">
-          <button
-            class="option"
-            :class="options.gender === Gender.BOY && 'option-active'"
-            @click="setGender(Gender.BOY)"
-          >
-            Boy
-          </button>
-
-          <button
-            class="option"
-            :class="options.gender === Gender.UNISEX && 'option-active'"
-            @click="setGender(Gender.UNISEX)"
-          >
-            Unisex
-          </button>
-
-          <button
-            class="option"
-            :class="options.gender === Gender.GIRL && 'option-active'"
-            @click="setGender(Gender.GIRL)"
-          >
-            Girl
-          </button>
-        </div>
-      </div>
-
-      <div class="option-container">
-        <h4>2) Choos the name's popularity</h4>
-
-        <div class="option-buttons">
-          <button
-            class="option"
-            :class="options.popularity === Popularity.TRENDY && 'option-active'"
-            @click="setPopularity(Popularity.TRENDY)"
-          >
-            Trendy
-          </button>
-
-          <button
-            class="option"
-            :class="options.popularity === Popularity.UNIQUE && 'option-active'"
-            @click="setPopularity(Popularity.UNIQUE)"
-          >
-            Unique
-          </button>
-        </div>
-      </div>
-
-      <div class="option-container">
-        <h4>3) Choos name length</h4>
-
-        <div class="option-buttons">
-          <button
-            class="option"
-            :class="options.length === Length.LONG && 'option-active'"
-            @click="setLength(Length.LONG)"
-          >
-            Long
-          </button>
-
-          <button
-            class="option"
-            :class="options.length === Length.ALL && 'option-active'"
-            @click="setLength(Length.ALL)"
-          >
-            All
-          </button>
-
-          <button
-            class="option"
-            :class="options.length === Length.SHORT && 'option-active'"
-            @click="setLength(Length.SHORT)"
-          >
-            Short
-          </button>
-        </div>
-      </div>
+      <Option
+        v-for="option in optionArray"
+        :key="option.title"
+        :option="option"
+        :options="options"
+      />
 
       <button class="primary" @click="computSelectedNames">Find Name's</button>
     </div>
@@ -167,31 +98,6 @@ const selectedNames = ref<string[]>([]);
 
 .option-container {
   margin-bottom: 2rem;
-}
-
-.option-buttons .option:first-child {
-  border-radius: 1rem 0 0 1rem;
-}
-
-.option-buttons .option:last-child {
-  border-radius: 0 1rem 1rem 0;
-}
-
-.option {
-  background-color: white;
-  outline: 0.15rem solid rgb(249, 87, 89);
-  border: none;
-  padding: 0.75rem;
-  width: 12rem;
-  font-size: 1rem;
-  color: rgb(27, 60, 138);
-  cursor: pointer;
-  font-weight: 300;
-}
-
-.option-active {
-  background-color: rgb(249, 87, 89);
-  color: white;
 }
 
 .primary {
---
+++ components/Option.vue
@@ -0,0 +1,64 @@
+import { Gender } from '~/data';
+<script setup lang="ts">
+import { Gender, Length, Popularity } from "@/data";
+
+interface OptionProps {
+  option: {
+    title: string;
+    category: string;
+    buttons: Gender[] | Popularity[] | Length[];
+  };
+  options: [gender: Gender, popularity: Popularity, length: Length];
+}
+
+const props = defineProps<OptionProps>();
+
+// Or
+// const props = defineProps({
+//   option: Object,
+//   options: Object,
+// });
+</script>
+
+<template>
+  <h4>{{ props.option.title }}</h4>
+
+  <div class="option-buttons">
+    <button
+      v-for="value in option.buttons"
+      :key="value"
+      class="option"
+      :class="options[option.category] === value && 'option-active'"
+      @click="options[option.category] = value"
+    >
+      {{ value }}
+    </button>
+  </div>
+</template>
+
+<style scoped>
+.option-buttons .option:first-child {
+  border-radius: 1rem 0 0 1rem;
+}
+
+.option-buttons .option:last-child {
+  border-radius: 0 1rem 1rem 0;
+}
+
+.option {
+  background-color: white;
+  outline: 0.15rem solid rgb(249, 87, 89);
+  border: none;
+  padding: 0.75rem;
+  width: 12rem;
+  font-size: 1rem;
+  color: rgb(27, 60, 138);
+  cursor: pointer;
+  font-weight: 300;
+}
+
+.option-active {
+  background-color: rgb(249, 87, 89);
+  color: white;
+}
+</style>
