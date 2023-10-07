377f1e7dbba690416cb427319601d38ba25b2247
ardalan.shahandeh <ardalan.shahandeh@gmail.com>
2023-10-07 23:56:49 +0330

    feat: Add Card Name Component

--- app.vue
+++ app.vue
@@ -62,13 +62,14 @@ const optionArray = [
     </div>
 
     <div class="card-container">
-      <div v-for="name in selectedNames" :key="name" class="card">
+      <CardName v-for="name in selectedNames" :key="name" :name="name" />
+      <!-- <div v-for="name in selectedNames" :key="name" class="card">
         <h4>
           {{ name }}
         </h4>
 
         <p>x</p>
-      </div>
+      </div> -->
     </div>
   </div>
 </template>
@@ -114,22 +115,5 @@ const optionArray = [
   display: flex;
   margin-top: 3rem;
   flex-wrap: wrap;
-}
-.card {
-  background-color: rgb(27, 60, 138);
-  width: 28%;
-  color: white;
-  border-radius: 1rem;
-  padding: 0.1rem;
-  margin-right: 0.5rem;
-  margin-bottom: 1rem;
-  position: relative;
-}
-.card p {
-  position: absolute;
-  top: -29%;
-  left: 92.5%;
-  cursor: pointer;
-  color: rgba(255, 255, 255, 0.178);
 }
 </style>
